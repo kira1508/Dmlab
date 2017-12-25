0d6f841c3fdcc228ee1ab8075aa119d3f7a0eab4
kira1508 <kiranhebbar96@gmail.com>
2017-12-25 21:28:03 +0530

    added record

---
+++ KNN/record.java
@@ -0,0 +1,13 @@
+
+public class record {
+	double dist,x,y,z;
+	String cla;
+	record(double dist,double x,double y,double z,String cla){
+		this.dist = dist;
+		this.x = x;
+		this.y =y;
+		this.z = z;
+		this.cla = cla;
+	}
+
+}
